8aacf6ce7786b4c0f10a4e6c2089457d30b41f7a
unknown <abhishekraj2447@gmail.com>
2025-08-23 01:53:11 +0530

    fix vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'  // agar Tailwind v4
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Tailwind plugin v4
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       assets: path.resolve(__dirname, './src/assets'),
       components: path.resolve(__dirname, './src/components'),
     },
+  },
+  build: {
+    outDir: 'dist', // 👈 Vercel will serve this
   },
 })
